a15753f2a80bce5572065b568afaed5817a5ac3f
XJP <xiao9410081121@163.com>
2019-10-11 14:39:26 +0800

    分布式配置中心

--- eureka_config_server/src/main/java/com/ahsj/eureka_config_server/EurekaConfigServerApplication.java
+++ eureka_config_server/src/main/java/com/ahsj/eureka_config_server/EurekaConfigServerApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableConfigServer
 public class EurekaConfigServerApplication {
 
