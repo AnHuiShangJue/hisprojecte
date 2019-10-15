4673b3afebe5fecfd0b9f8f3092eaa3342dba063
XJP <xiao9410081121@163.com>
2019-10-15 20:06:14 +0800

    提交

--- pay-alipay/src/main/java/com/ahsj/payalipay/PayAlipayApplication.java
+++ pay-alipay/src/main/java/com/ahsj/payalipay/PayAlipayApplication.java
@@ -2,29 +2,34 @@ package com.ahsj.payalipay;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringCloudApplication
 @EnableDiscoveryClient
-@MapperScan("com.ahsj.payAlipay.dao")
-@ComponentScan(basePackages = "com.ahsj")
+@MapperScan("com.ahsj.payalipay.dao")
+@SpringBootApplication(scanBasePackages = {"com.ahsj.payalipay","com.ahsj.payalipay.error"})
 @EnableTransactionManagement //开启事务
 @EnableCaching //开启缓存
-@EnableFeignClients
+//开启Feign客户端配置
+@EnableFeignClients(value = "com.ahsj.payalipay")
+//开启任务处理
+@EnableScheduling
+@RefreshScope
 public class PayAlipayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PayAlipayApplication.class, args);
     }
 
-    @PostMapping("/api/hi")
-    public String getHi(){
-        return "订立";
-    }
 
 }
