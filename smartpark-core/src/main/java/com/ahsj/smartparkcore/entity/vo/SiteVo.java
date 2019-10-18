4d4b722e8251f917d6ad641a204bc0b7ced8b061
XJP <xiao9410081121@163.com>
2019-10-18 18:06:13 +0800

    提交

--- smartpark-core/src/main/java/com/ahsj/smartparkcore/entity/vo/SiteVo.java
+++ smartpark-core/src/main/java/com/ahsj/smartparkcore/entity/vo/SiteVo.java
@@ -5,183 +5,34 @@ import com.ahsj.smartparkcore.entity.po.Site;
 import java.math.BigDecimal;
 
 public class SiteVo extends Site {
-    private Long id;
 
-    private String siteName;
+    private Long provinceId;
 
-    private String title;
+    private Long cityId;
 
-    private String location;
+    private Long areaId;
 
-    private BigDecimal price;
-
-    private Double area;
-
-    private String name;
-
-    private String phoneNumber;
-
-    private String description;
-
-    private Short isEnable;
-
-    private String capacity;
-
-    private String environmentalLabel;
-
-    private Integer isLease;
-
-    private Integer isVerify;
-
-    private Long enterpriseId;
-
-    @Override
-    public Long getId() {
-        return id;
+    public Long getProvinceId() {
+        return provinceId;
     }
 
-    @Override
-    public void setId(Long id) {
-        this.id = id;
+    public void setProvinceId(Long provinceId) {
+        this.provinceId = provinceId;
     }
 
-    @Override
-    public String getSiteName() {
-        return siteName;
+    public Long getCityId() {
+        return cityId;
     }
 
-    @Override
-    public void setSiteName(String siteName) {
-        this.siteName = siteName;
+    public void setCityId(Long cityId) {
+        this.cityId = cityId;
     }
 
-    @Override
-    public String getTitle() {
-        return title;
+    public Long getAreaId() {
+        return areaId;
     }
 
-    @Override
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    @Override
-    public String getLocation() {
-        return location;
-    }
-
-    @Override
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    @Override
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    @Override
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    @Override
-    public Double getArea() {
-        return area;
-    }
-
-    @Override
-    public void setArea(Double area) {
-        this.area = area;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    @Override
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    @Override
-    public String getDescription() {
-        return description;
-    }
-
-    @Override
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public Short getIsEnable() {
-        return isEnable;
-    }
-
-    @Override
-    public void setIsEnable(Short isEnable) {
-        this.isEnable = isEnable;
-    }
-
-    @Override
-    public String getCapacity() {
-        return capacity;
-    }
-
-    @Override
-    public void setCapacity(String capacity) {
-        this.capacity = capacity;
-    }
-
-    @Override
-    public String getEnvironmentalLabel() {
-        return environmentalLabel;
-    }
-
-    @Override
-    public void setEnvironmentalLabel(String environmentalLabel) {
-        this.environmentalLabel = environmentalLabel;
-    }
-
-    @Override
-    public Integer getIsLease() {
-        return isLease;
-    }
-
-    @Override
-    public void setIsLease(Integer isLease) {
-        this.isLease = isLease;
-    }
-
-    @Override
-    public Integer getIsVerify() {
-        return isVerify;
-    }
-
-    @Override
-    public void setIsVerify(Integer isVerify) {
-        this.isVerify = isVerify;
-    }
-
-    @Override
-    public Long getEnterpriseId() {
-        return enterpriseId;
-    }
-
-    @Override
-    public void setEnterpriseId(Long enterpriseId) {
-        this.enterpriseId = enterpriseId;
+    public void setAreaId(Long areaId) {
+        this.areaId = areaId;
     }
 }
