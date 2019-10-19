506ab999443c8f65458c63104d03107ee2c3370d
zsx <2529207109@qq.com>
2019-10-19 14:00:25 +0800

    微信openid返回map类型

--- base-user-info/src/main/java/com/ahsj/baseuserinfo/controller/UserInfoController.java
+++ base-user-info/src/main/java/com/ahsj/baseuserinfo/controller/UserInfoController.java
@@ -2,6 +2,7 @@ package com.ahsj.baseuserinfo.controller;
 
 import com.ahsj.baseuserinfo.entity.UserInfo;
 import com.ahsj.baseuserinfo.services.UserService;
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import core.entity.PageBean;
 import core.message.Message;
@@ -15,6 +16,8 @@ import utils.EmptyUtil;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.NotNull;
 import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/api/user")
@@ -151,7 +154,9 @@ public class UserInfoController extends BaseOAController{
         }
         System.out.println("openid:" + openid);
         System.out.println("sr:" + sr);
-        return openid;
+        Map map = new HashMap();
+        map.put("openid",openid);
+        return JSON.toJSONString(map);
     }
 }
 
