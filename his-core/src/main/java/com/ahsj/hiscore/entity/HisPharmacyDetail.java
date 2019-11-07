7645bb78405925d2da4f994053b1591012f772e5
XJP <xiao9410081121@163.com>
2019-11-07 16:32:10 +0800

    提交

--- his-core/src/main/java/com/ahsj/hiscore/entity/HisPharmacyDetail.java
+++ his-core/src/main/java/com/ahsj/hiscore/entity/HisPharmacyDetail.java
@@ -146,6 +146,32 @@ public class HisPharmacyDetail extends BaseEntity {
     private Long departmentId;
     private String departmentIdName;
 
+    private String deptName;
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentIdName() {
+        return departmentIdName;
+    }
+
+    public void setDepartmentIdName(String departmentIdName) {
+        this.departmentIdName = departmentIdName;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
     public Integer getStockNum() {
         return stockNum;
     }
