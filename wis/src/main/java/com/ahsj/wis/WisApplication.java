71360c70151e5e0f793d3730891414b2d4430552
dingli <dingli@anhuishangjue.com>
2019-11-13 15:18:07 +0800

    提交代码

--- wis/src/main/java/com/ahsj/wis/WisApplication.java
+++ wis/src/main/java/com/ahsj/wis/WisApplication.java
@@ -1,9 +1,25 @@
 package com.ahsj.wis;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringCloudApplication
+@EnableDiscoveryClient
+@MapperScan("com.ahsj.wis.dao")
+@SpringBootApplication(scanBasePackages = {"com.ahsj.wisdom", "com.ahsj.wis.error"})
+@EnableTransactionManagement //开启事务
+@EnableCaching //开启缓存
+@EnableFeignClients(value = "com.ahsj.wis")
+@EnableScheduling
+@RefreshScope
 public class WisApplication {
 
     public static void main(String[] args) {
